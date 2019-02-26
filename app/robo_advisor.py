820522fed2d8ccfe64d5054ff8482ea9d392f28d
kmalhotra13 <k9@sukarm.com>
2019-02-25 22:26:42 -0500

    Update robo_advisor.py

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -273,30 +273,40 @@ for k, v in parsed_index_data['Time Series (Daily)'].items():
 
 # Benchmark calculations: 
 
-index_sigma = stat.stdev(index_close_price)
-index_xbar = stat.mean(index_close_price)
+index_daily_delta = [index_close_price[n] / index_close_price[n-1] - 1 for n in range(1,len(index_close_price))]
+
+
+
+index_sigma = stat.stdev(index_daily_delta)
+index_xbar = stat.mean(index_daily_delta)
 index_coeff = index_sigma/index_xbar
 index_sharpe = index_xbar / index_sigma
+index_sharpe_str = str("{0:,.2f}".format(index_sharpe))
 
 
 # Recommendation engine:
 
-sigma = stat.stdev(close_price)
-xbar = stat.mean(close_price)
+daily_delta = [close_price[n] / close_price[n-1] - 1 for n in range(1,len(close_price))]
+print(daily_delta)
+
+	
+sigma = stat.stdev(daily_delta)
+xbar = stat.mean(daily_delta)
 coeff = sigma/xbar
 sharpe = xbar / sigma
+sharpe_str = str("{0:,.2f}".format(sharpe))
 
 if sharpe > index_sharpe:
 	rec_sum = "Buy!"
-	rec_exp = f"The Sharpe ratio of {symbol} ({str("{0:,.2f}".format(sharpe))}) is greater than the Sharpe ratio of the benchmark {index_ticker} ({str("{0:,.2f}".format(index_sharpe))})" 
+	rec_exp = f"The Sharpe ratio of {symbol} ({sharpe_str}) is greater than the Sharpe ratio of the benchmark {index_ticker} ({index_sharpe_str})." 
 	rec_exp2 = f"This means that you can gain equivalent returns with less risk by investing in {symbol}."
 elif sharpe < index_sharpe:
 	rec_sum = "Sell!"
-	rec_exp = f"The Sharpe ratio of {symbol} ({str("{0:,.2f}".format(sharpe))}) is less than the Sharpe ratio of the benchmark {index_ticker} ({str("{0:,.2f}".format(index_sharpe))})" 
+	rec_exp = f"The Sharpe ratio of {symbol} ({sharpe_str}) is less than the Sharpe ratio of the benchmark {index_ticker} ({index_sharpe_str})." 
 	rec_exp2 = f"This means that you can gain equivalent returns with less risk by investing in {index_ticker}."
-elif sharpe = index_sharpe:
+elif sharpe == index_sharpe:
 	rec_sum = "Hold!"
-	rec_exp = f"The Sharpe ratio of {symbol} ({str("{0:,.2f}".format(sharpe))}) is equal to the Sharpe ratio of the benchmark {index_ticker} ({str("{0:,.2f}".format(index_sharpe))})" 
+	rec_exp = f"The Sharpe ratio of {symbol} ({sharpe_str}) is equal to the Sharpe ratio of the benchmark {index_ticker} ({index_sharpe_str})." 
 	rec_exp2 = f"This means that you can gain equivalent returns with equivalent risk by investing in either."
 
 # TODO: further revise the example outputs below to reflect real information
